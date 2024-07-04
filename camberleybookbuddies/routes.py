6af4fce7a255d88d240c17b5903b41a0cba32829
Jordan Cook <jordancook94@live.co.uk>
2024-07-04 15:13:33 +0000

    Build routes for links in base.html header

--- camberleybookbuddies/routes.py
+++ camberleybookbuddies/routes.py
@@ -6,3 +6,25 @@ from camberleybookbuddies.models import Book, Review
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/library")
+def library():
+    return render_template("library.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    return render_template("add_book.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
