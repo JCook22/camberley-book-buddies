2787863d0c4f2c8c5df70fc4a906cb6d01791faa
Jordan Cook <jordancook94@live.co.uk>
2024-07-07 17:11:33 +0000

    Add functionality to view library and add books to it

--- camberleybookbuddies/models.py
+++ camberleybookbuddies/models.py
@@ -9,13 +9,13 @@ class Book(db.Model):
     reviews = db.relationship("Review", backref="book", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        return self.book_name
+        return self.book_title
 
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey("book.id", ondelete="CASCADE"), nullable=False)
-    book_name = db.Column(db.String(50), unique=True, nullable=False)
+    book_name = db.Column(db.String(50), nullable=False)
     review_author = db.Column(db.String(50), nullable=False)
     review_headline = db.Column(db.String(75), nullable=False)
     review_description = db.Column(db.Text, nullable=False)
--- camberleybookbuddies/routes.py
+++ camberleybookbuddies/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from camberleybookbuddies import app, db
 from camberleybookbuddies.models import Book, Review
 
@@ -10,17 +10,23 @@ def home():
 
 @app.route("/library")
 def library():
-    return render_template("library.html")
+    library = list(Book.query.order_by(Book.book_title).all())
+    return render_template("library.html", library=library)
+
+
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    if request.method == "POST":
+        book = Book(book_title=request.form.get("book_title"), book_author=request.form.get("book_author"), book_genre=request.form.get("book_genre"))
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for("add_book"))
+    return render_template("add_book.html")
 
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
     return render_template("search.html")
-
-
-@app.route("/add_book", methods=["GET", "POST"])
-def add_book():
-    return render_template("add_book.html")
 
 
 @app.route("/contact")
