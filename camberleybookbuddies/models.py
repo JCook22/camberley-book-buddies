ad0fa27c23807b95f50ae3b9eccab51a276048ec
Jordan Cook <jordancook94@live.co.uk>
2024-07-23 20:01:09 +0000

    Shorten line length where exceeding 80 characters

--- camberleybookbuddies/models.py
+++ camberleybookbuddies/models.py
@@ -6,7 +6,8 @@ class Book(db.Model):
     book_title = db.Column(db.String(50), nullable=False)
     book_author = db.Column(db.String(50), nullable=False)
     book_genre = db.Column(db.String(20), nullable=False)
-    reviews = db.relationship("Review", backref="book", cascade="all, delete", lazy=True)
+    reviews = db.relationship(
+        "Review", backref="book", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         return self.book_title
@@ -14,10 +15,11 @@ class Book(db.Model):
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey("book.id", ondelete="CASCADE"), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey(
+        "book.id", ondelete="CASCADE"), nullable=False)
     review_author = db.Column(db.String(50), nullable=False)
     review_headline = db.Column(db.String(75), nullable=False)
     review_description = db.Column(db.Text, nullable=False)
-    
+
     def __repr__(self):
-        return f"#{self.id} - Author:{self.review_author} | Headline:{self.review_headline}"
+        return f"#{self.id} - Headline:{self.review_headline}"
